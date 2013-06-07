a46ad25ed79e65da2d370db86b7302db971ba281
franceschi <g.franceschi@tmg.nl>
2013-06-07 15:21:06 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python2.7
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='pinhead',
       version='0.1.1',
       description='A tool for auto-pinning kvm vcpus to physical threads on the local machine',
       author='Giorgio Franceschi',
       author_email='g.franceschi@tmg.nl',
-      packages=find_packages(),
+      packages=['pinhead'],
 )
